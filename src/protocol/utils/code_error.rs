aedef9026c6c86543609f44742f86176d7aebccf
PorterZhang2021 <porterzhang2021@outlook.com>
2025-11-15 16:42:43 +0800

    style: format style

--- src/protocol/utils/code_error.rs
+++ src/protocol/utils/code_error.rs
@@ -6,10 +6,10 @@ pub(crate) enum CodeError {
     #[error("Code conversion error: {0}")]
     CodeConversionError(String),
 
-    #[error("code length error: expected {0}, got {1}")]
+    #[error("Code length error: expected {0}, got {1}")]
     CodeLengthError(usize, usize),
 
-    #[error("usize conversion error: value {0} is out of range for {1}")]
+    #[error("Usize conversion error: value {0} is out of range for {1}")]
     UsizeConversionError(usize, &'static str),
 
     #[error("UTF-8 decoding error")]
